aaa491f7e343d69727c38bbf031c004331652c97
alepapadop <alepapadop@gmail.com>
2024-02-11 23:49:55 +0200

    clear the files

---
+++ CmdArgsCppMain.cpp
@@ -0,0 +1,15 @@
+#include "CmdArgsCpp.hpp"
+
+
+int main()
+{
+
+    //CmdArgsCpp cmd_args;
+
+    //cmd_args.AddArgument(std::string("i"));
+
+    //cmd_args.DebugArgs();
+
+
+    return EXIT_SUCCESS;
+}
